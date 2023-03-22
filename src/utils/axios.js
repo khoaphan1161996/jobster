7b6b206f47f22a1fd02c6d9bec23eda826fa77a2
khoaphan1161996 <khoa071996@gmail.com>
2023-03-22 15:03:54 +0700

    refactor thunk and bearer with axios interceptor

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,16 @@
 import axios from "axios";
+import { getUserToLocalStorage } from "./localStorage.js";
 
 const customFetch = axios.create({
   baseURL: "https://jobify-prod.herokuapp.com/api/v1/toolkit",
+});
+
+customFetch.interceptors.request.use((config) => {
+  const user = getUserToLocalStorage();
+  if (user) {
+    config.headers["Authorization"] = `Bearer ${user.token}`;
+  }
+  return config;
 });
 
 export default customFetch;
